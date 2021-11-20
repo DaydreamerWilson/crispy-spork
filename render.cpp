3e5b22624c63a88c41576d4b07e1244d9177e92d
Wilson <wilson02715@gmail.com>
2021-11-20 22:27:33 +0800

    Added functions for render.h
    Created functions for class renderer

--- render.cpp
+++ render.cpp
@@ -1,1 +1,65 @@
+#include "render.h"
+#include <iostream>
 
+/*
+class renderer
+{
+private:
+  char ** pixel
+  int height, width;
+}
+*/
+
+renderer::renderer(int h, int w){
+  height = h;
+  width = w;
+  
+  pixel = new char * [height];
+  for(int i = 0; i < height; i++){
+    pixel[i] = new char[width];
+  }
+}
+
+renderer::resize(int h, int w){
+  for(int i = 0; i < height; i++){
+    delete[] pixel[i];
+  }
+  delete[] pixel;
+  
+  height = h;
+  width = w;
+  
+  pixel = new char * [height];
+  for(int i = 0; i < height; i++){
+    pixel[i] = new char[width];
+  }
+}
+  
+renderer::drawPoint(int x, int y, char c){
+  pixel[y][x]=c;
+}
+  
+renderer::drawLine(int x1, int y1, int x2, int y2, char c){
+  
+}
+  
+renderer::drawRectangle(int x, int y, int h, int w, char c){
+  
+}
+  
+renderer::present(){
+  for(int i = 0; i < height; i++){
+    for(int j = 0; j < width; j++){
+      cout << pixel[i][j];
+    }
+    cout << endl;
+  }
+}
+
+renderer::clear(){
+  for(int i = 0; i < height; i++){
+    for(int j = 0; j < width; j++){
+      pixel[i][j]='\0';
+    }
+  }
+}
