555f106002c47efc072f45307b92647da11e8096
Wilson <wilson02715@gmail.com>
2021-11-21 01:47:28 +0800

    implemented int_to_string()

--- library.cpp
+++ library.cpp
@@ -1,5 +1,17 @@
+#include <cmath>
 #include "library.h"
 
 int sel_resol = small;
 
-string int_to_string(int k);
+string int_to_string(int k){
+  int i = 0;
+  string temp;
+  while(pow(10, i)<k){
+    i++;
+  }
+  for(int j = i; j > 0; j--){
+    temp[j-i] = k / pow(10, j);
+    k %= pow(10, j);
+  }
+  return temp;
+}
