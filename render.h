a38d367adc15628c0899456b12726212e4ee0d97
Wilson <wilson02715@gmail.com>
2021-11-20 23:18:03 +0800

    Updated render.h after debugging render.cpp

--- render.h
+++ render.h
@@ -5,23 +5,15 @@ class renderer
 {
 private:
   char ** pixel = NULL;
-  int height, width;
 public:
+  int height, width;
   void renderer(int h, int w);
-  
   void resize(int h, int w);
-  
   bool drawPoint(int x, int y, char c);
-  
   bool drawLine(int x1, int y1, int x2, int y2, char c);
-  
   bool drawRectangle(int x, int y, int h, int w, char c);
-  
   bool present();
-  
   void clear();
-  
-  int[2] size();
 }
 
 #endif
