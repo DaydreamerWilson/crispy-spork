76f8c9f4cf97450a5ac76ed964aa4b5cf63bcca4
Wilson <wilson02715@gmail.com>
2021-11-20 22:10:32 +0800

    Update render.h
    Added renderer class for drawing texts on the console

--- render.h
+++ render.h
@@ -1,4 +1,24 @@
 #ifndef render
 #define render
 
+class renderer
+{
+private:
+  char ** pixel = NULL;
+public:
+  void renderer(int h, int w);
+  
+  void resize(int h, int w);
+  
+  bool drawPoint(int x, int y, char c);
+  
+  bool drawLine(int x1, int y1, int x2, int y2, char c);
+  
+  bool drawRectangle(int x, int y, int h, int w, char c);
+  
+  bool present();
+  
+  int[2] size();
+}
+
 #endif
