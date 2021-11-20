21ff85cfa2d1692953d6e8c3c56b0d2c2261f85c
Wilson <wilson02715@gmail.com>
2021-11-20 23:21:43 +0800

    Debugged main.cpp, fool-proofed resolution calibration

--- main.cpp
+++ main.cpp
@@ -12,16 +12,18 @@ int iinput[5];
 int main(){
   sinput = "\0";
   while(sinput!="OKAY"){
-    cout << "Please enter desired resolution (height width):
+    cout << "Please enter desired resolution (height width):";
     cin >> iinput[0] >> iinput[1];
-    renderer0.resize(iinput[0], iinput[1]);
-    renderer0.drawPoint(0, 0, '*');
-    renderer0.drawPoint(iinput[0], 0, '*');
-    renderer0.drawPoint(0, iinput[1], '*');
-    renderer0.drawPoint(iinput[0], iinput[1], '*');
-    renderer0.present();
-    renderer0.clear();
-    cout << "Please make sure you can see this line and all 4 * simultaneously." << endl;
-    cout << "Enter 'OKAY' to confirm, anything otherwise to change resolution:"
+    if((iinput[0]>0 && iinput[0]<200) && (iinput[1]>0 && iinput[1]<200)){
+      renderer0.resize(iinput[0], iinput[1]);
+      renderer0.drawPoint(0, 0, '*');
+      renderer0.drawPoint(iinput[1]-1, 0, '*');
+      renderer0.drawPoint(0, iinput[0]-1, '*');
+      renderer0.drawPoint(iinput[1]-1, iinput[0]-1, '*');
+      renderer0.present();
+      renderer0.clear();
+      cout << "Please make sure you can see this line and all 4 * simultaneously." << endl;
+      cout << "Enter 'OKAY' to confirm, anything otherwise to change resolution:";
+    }
   }
 }
