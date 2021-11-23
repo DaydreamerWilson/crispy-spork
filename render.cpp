1fbc61f7830cf4265877b1cf6f1e94b4474ab884
Tang <77083401+CircleLight@users.noreply.github.com>
2021-11-23 19:12:45 +0800

    Update render.cpp

--- render.cpp
+++ render.cpp
@@ -41,13 +41,61 @@ bool renderer::drawPoint(int x, int y, char c){
     return false;
   }
 }
-
+//New
 bool renderer::drawLine(int x1, int y1, int x2, int y2, char c){
-  return false;
+  if ((x1!=x2)&&(y1!=y2)){
+    cout << "error drawing line: line not vertical or horizontal" << endl;
+    return false;
+  }
+  else if (x1>=0 && x2>=0){
+    if (x1>=resolution[sel_resol][const_h]||x2>=resolution[sel_resol][const_h]) || (y1>=resolution[sel_resol][const_w]||y2>=resolution[sel_resol][const_w])){
+      cout << "error drawing line: line out of bound" << endl;
+      return false;
+      }
+    }
+  else if (x1<0 && x2>0){
+    if (x1<=resolution[sel_resol][const_h]||x2>=resolution[sel_resol][const_h]) || (y1>=resolution[sel_resol][const_w]||y2>=resolution[sel_resol][const_w])){
+      cout << "error drawing line: line out of bound" << endl;
+      return false;
+    }
+  else if (x1>0 && x2<0){
+    if (x1>=resolution[sel_resol][const_h]||x2<=resolution[sel_resol][const_h]) || (y1>=resolution[sel_resol][const_w]||y2>=resolution[sel_resol][const_w])){
+      cout << "error drawing line: line out of bound" << endl;
+      return false;
+    }
+  else{
+    if (x1<=resolution[sel_resol][const_h]||x2<=resolution[sel_resol][const_h]) || (y1>=resolution[sel_resol][const_w]||y2>=resolution[sel_resol][const_w])){
+      cout << "error drawing line: line out of bound" << endl;
+      return false;
+      }
+    }
+  }
+  else{
+    if (y1==y2){
+      for (int i; i<x2; i++){
+        drawPoint(i, y1, c);
+      }
+    }
+    else if (x1==x2){
+      for (int j; j<y2; j++){
+        drawPoint(x1, j, c);
+      }
+    }
+  }
 }
-
+//New
 bool renderer::drawRectangle(int x, int y, int h, int w, char c){
-  return false;
+  double x_left=x, x_right=x_left+w;
+  double y_lower=y, y_higher=y_lower+h;
+  double height = resolution[sel_resol][const_h];
+  double width = resolution[sel_resol][const_w];
+  if ((h>height)||(w>width)){
+    cout << "error drawing rectangle: rectangle out of bound";
+    return false;
+  }
+  else{
+    drawLine(x_left, y_lower, x_right, y_higher, c);
+  }
 }
 
 bool renderer::drawText(int x, int y, string s){
