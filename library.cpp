162f3145335c2baea17185d0cffeb6d22da91634
Wilson <wilson02715@gmail.com>
2021-11-26 13:08:36 +0800

    Add files via upload

--- library.cpp
+++ library.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <time.h>
 #include "library.h"
 
 using namespace std;
@@ -15,19 +17,27 @@ map::map(string fin){
   ifstream file;
   file.open(fin);
   if(file.fail()){cout << "fatal error loading map: file failed" << endl;}
+  string temp;
 
-  file >> height >> width;
+  getline(file, temp);
+  istringstream sin (temp);
+  sin >> height >> width;
+  cout << temp << ' ';
 
   grid = new char * [height];
 
   for(int i = 0; i < height; i++){
     grid[i] = new char [width];
+    getline(file, temp);
+    cout << temp << ' ';
     for(int j = 0; j < width; j++){
-      file >> grid[i][j];
+      grid[i][j]=temp[j];
+      //cout << grid[i][j];
     }
+    //cout << endl;
   }
 
-  cout << "Successfully loaded map: " << fin << endl;
+  cout << endl << "Successfully loaded map: " << fin << endl;
 }
 
 string int_to_string(int k){
@@ -41,4 +51,50 @@ string int_to_string(int k){
     k %= (int)pow(10, j);
   }
   return temp;
+}
+
+void init_rand(){
+  srand(time(0));
+  int sumi, tempi;
+  double sumd, tempd, vi, vd;
+
+  sumi = 0;
+  sumd = 0;
+  vi = 0;
+  vd = 0;
+  cout << "testing random number generation" << endl;
+
+  for(int i=0; i<1000; i++){
+    tempd = randDouble(0, 1);
+    sumd += tempd;
+    vd += pow(tempd-0.5, 2);
+  }
+  cout << "mean(0.5)/variance(m=0.5)(0.083) of 1000 double generation:" << sumd/1000 << '/' << vd/999 << endl;
+
+  for(int i=0; i<1000; i++){
+    tempi = randInt(0, 11);
+    sumi += tempi;
+    vi += pow((double)tempi-5, 2);
+  }
+  cout << "mean(5)/variance(m=5)(10.000) of 1000 integer generation:" << (double)sumi/1000 << '/' << vi/999 << endl;
+}
+
+int randInt(int min, int max){
+  return rand() % (max-min) + min;
+}
+
+double randDouble(double min, double max){
+  double temp = 0;
+  double range = max-min;
+  int i=0;
+  while(pow(10, i)<range){
+    i++;
+  }
+  for(int j = -3; j<i; j++){
+    temp+=randInt(0,10)*pow(10, j);
+  }
+  while(temp>=range){
+    temp-=range;
+  }
+  return temp+min;
 }
--- library.h
+++ library.h
@@ -27,7 +27,21 @@ public:
   char ** grid;
   map(string fin);
 };
+// storing ascii art or map files
 
 string int_to_string(int k);
+// converting integer to string
+
+void init_rand();
+// reset seed for random number generation
+// test generation randomness
+
+int randInt(int min, int max);
+// generate random integers with minimum value (inclusive) and maximum (exclusive)
+// randInt(0,3) will generate three integers 0, 1, 2
+
+double randDouble(double min, double max);
+// generate random double with minimum value (inclusive) and maximum (exclusive) or 3 decimal places
+// randDouble(0, 3) will generate from 0.000 to 2.999
 
 #endif
